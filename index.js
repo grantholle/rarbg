19675d72d368f802b46033c75f3b2b14988fb1ef
Chris Alderson <chrisalderson@protonmail.com>
2017-09-12 13:21:53 +0200

    cleanup the index

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 'use strict'
 
-const https = require('https'),
-      querystring = require('querystring'),
-      moment = require('moment'),
-      config = {
-        host: 'torrentapi.org',
-        path: '/pubapi_v2.php?'
-      }
-
+const https = require('https')
+const moment = require('moment')
+const { stringify } = require('querystring')
 
 module.exports = {
+  config: {
+    host: 'torrentapi.org',
+    path: '/pubapi_v2.php?'
+  },
 
   categories: {
     XXX: 4,
@@ -36,115 +35,98 @@ module.exports = {
   lastRequestTime: moment(),
   tokenTimestamp: moment('1970-01-01', 'YYYY-MM-DD'),
 
-  validateParams(query) {
+  validateParams (query) {
     return new Promise((resolve, reject) => {
       if (!query.mode) {
-        return reject('Invalid query object -- no search mode')
+        return reject(new Error('Invalid query object -- no search mode'))
       } else if (query.mode === 'search') {
-        if (!query.search_string &&
-            !query.search_themoviedb &&
-            !query.search_tvdb &&
-            !query.search_imdb) {
-
-          return reject('Invalid query object -- no search parameters')
+        if (
+          !query.search_string &&
+          !query.search_themoviedb &&
+          !query.search_tvdb &&
+          !query.search_imdb
+        ) {
+          const err = new Error('Invalid query object -- no search parameters')
+          return reject(err)
         }
       } else if (query.mode !== 'list') {
-        return reject('Invalid query object -- search mode invalid')
+        return reject(new Error('Invalid query object -- search mode invalid'))
       }
 
       resolve()
     })
   },
 
-  setToken() {
-    this._setting_token = true
-
+  setToken () {
     return this.sendRequest({
       get_token: 'get_token'
-    }).then(response => {
-      this._token = response.token
-      this._setting_token = false
+    }).then(res => {
+      this._token = res.token
       this.tokenTimestamp = moment()
     })
   },
 
-  getToken() {
-    if ((!this._token && !this._setting_token) || (moment().diff(this.tokenTimestamp, 'minutes') > 14))
+  getToken () {
+    if (
+      (!this._token && !this._setting_token) ||
+      (moment().diff(this.tokenTimestamp, 'minutes') > 14)
+    ) {
       return this.setToken()
+    }
 
-    return new Promise((resolve, reject) => {
-      if (this._token) {
-        return resolve()
-      }
-
-      (function waitForToken() {
-        if (!this._setting_token)
-          return resolve()
-
-        setTimeout(waitForToken.bind(this), 100)
-      }).bind(this)()
-    }).then(() => true)
+    return Promise.resolve()
   },
 
-  search(query) {
+  search (query) {
     query.mode = 'search'
-
     return this.apiRequest(query)
   },
 
-  list(query = {}) {
+  list (query = {}) {
     query.mode = 'list'
-
     return this.apiRequest(query)
   },
 
-  apiRequest(query) {
+  apiRequest (query) {
     return new Promise((resolve, reject) => {
-      Promise.all([this.validateParams(query), this.getToken()])
-        .then(() => {
-          // There's a 1 request/2 second rate limit
-          const delay = 2000 - moment().diff(this.lastRequestTime)
-          query.token = this._token
+      return Promise.all([
+        this.validateParams(query),
+        this.getToken()
+      ]).then(() => {
+        // There's a 1 request/2 second rate limit
+        const delay = 2000 - moment().diff(this.lastRequestTime)
+        query.token = this._token
 
-          setTimeout(() => {
-            this.sendRequest(query)
-              .then(results => {
-                if (results.torrent_results)
-                  resolve(results.torrent_results)
-                else
-                  reject(results)
-              })
-              .catch(e => reject(e))
-          }, delay > 0 ? delay : 0)
-        })
+        return setTimeout(() => {
+          return this.sendRequest(query).then(({ torrent_results }) => {
+            if (!torrent_results) { // eslint-disable-line camelcase
+              return reject(new Error('No results found!'))
+            }
+
+            return resolve(torrent_results)
+          }).catch(err => reject(err))
+        }, delay)
+      }).catch(err => reject(err))
     })
   },
 
-  sendRequest(query) {
+  sendRequest (query) {
     return new Promise((resolve, reject) => {
       const req = {
-        host: config.host,
-        path: config.path + querystring.stringify(query)
+        host: this.config.host,
+        path: this.config.path + stringify(query)
       }
 
       https.get(req, res => {
         this.lastRequestTime = moment()
         let body = ''
 
+        res.setEncoding('utf8')
         res.on('data', d => {
           body += d
         })
-
-        res.on('end', () => {
-          resolve(JSON.parse(body))
-        })
-
-        res.on('error', e => {
-          reject(e)
-        })
-
-      })
+        res.on('end', () => resolve(JSON.parse(body)))
+      }).on('error', err => reject(err))
     })
   }
-
 }
