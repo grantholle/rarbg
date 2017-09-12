2cffd424e35e05ab0d895f0b5311f5279a6e3a8d
Chris Alderson <chrisalderson@protonmail.com>
2017-09-12 12:59:52 +0200

    add some test cases for the module

---
+++ test/index.spec.js
@@ -0,0 +1,102 @@
+'use strict'
+
+const { expect } = require('chai')
+const rarbg = require('..')
+
+describe('rarbg', () => {
+  function testOutputAttributes (output) {
+    expect(output).to.be.an('array')
+
+    const random = Math.floor(Math.random() * output.length)
+    const toTest = output[random]
+
+    expect(toTest.filename).to.be.a('string')
+    expect(toTest.category).to.be.a('string')
+    expect(toTest.download).to.be.a('string')
+  }
+
+  it('should search for torrents', done => {
+    rarbg.search({
+      search_string: 'star wars',
+      sort: 'seeders',
+      category: rarbg.categories.MOVIES_X264_1080,
+      min_seeders: 50
+    }).then(response => {
+      testOutputAttributes(response)
+      done()
+    }).catch(done)
+  })
+
+  it('should throw an error when ', done => {
+    rarbg.search({
+      search_strin: 'star wars'
+    }).then(done)
+      .catch(err => {
+        expect(err).to.be.an('Error')
+        expect(err.message).to.equal('Invalid query object -- no search parameters')
+
+        done()
+      })
+  })
+
+  it('should list the recent torrent', done => {
+    rarbg.list().then(response => {
+      testOutputAttributes(response)
+      done()
+    }).catch(done)
+  })
+
+  it('should throw an error when no params are given', done => {
+    rarbg.validateParams({})
+      .then(done)
+      .catch(err => {
+        expect(err).to.be.an('Error')
+        expect(err.message).to.equal('Invalid query object -- no search mode')
+
+        done()
+      })
+  })
+
+  it('should throw an error when the wrong params are given', done => {
+    rarbg.validateParams({
+      mode: 'search',
+      search_strin: 'star wars'
+    }).then(done)
+      .catch(err => {
+        expect(err).to.be.an('Error')
+        expect(err.message).to.equal('Invalid query object -- no search parameters')
+
+        done()
+      })
+  })
+
+  it('should throw an error a faulty mode is given', done => {
+    rarbg.validateParams({
+      mode: 'faulty'
+    }).then(done)
+      .catch(err => {
+        expect(err).to.be.an('Error')
+        expect(err.message).to.equal('Invalid query object -- search mode invalid')
+
+        done()
+      })
+  })
+
+  it('should throw an error when a request is not valid', done => {
+    const temp = rarbg.config
+    rarbg.config = {
+      host: 'somefaultyhost.com',
+      path: '/somefaultypath.php?'
+    }
+
+    rarbg.search({
+      search_string: 'star wars'
+    }).then(done)
+      .catch(err => {
+        expect(err).to.be.an('Error')
+
+        rarbg.config = temp
+        done()
+      })
+  })
+})
